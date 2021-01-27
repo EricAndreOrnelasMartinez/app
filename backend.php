329e722dcf8a382066b27a3837e04b048255b1c0
ericcot2@gmail.com <ericcot2@gmail.com>
2021-01-27 13:17:55 -0600

    add

--- backend.php
+++ backend.php
@@ -1,13 +1,19 @@
 <?php
-
-$nombre = $_POST['$Nombre'];
+include('con.php');
+$nombre = $_POST['Nombre'];
 $apeido = $_POST['Apeido'];
 $edad = $_POST['edad'];
 
 if($nombre === '' || $apeido === '' || $edad === ''){
-    echo json_encode('todo mal');
+    echo json_encode('completar campos');
 }else{
-    echo json_encode('todo bien');
+    $result = mysqli_query("INSERT INTO personas(Nombre,Apeido,Edad) VALUES('$nombre','$apeido','$edad');", $con);
+    if($result){
+        echo json_encode('Registrado');
+    }else{
+        echo json_encode('algo falló');
+    }
+
 }
 
 ?>
