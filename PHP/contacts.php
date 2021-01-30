b217b5151a6832399460bd247a97c69e490af6a1
eric <ericcot2@gmail.com>
2021-01-30 17:05:33 -0600

    add

--- PHP/contacts.php
+++ PHP/contacts.php
@@ -5,5 +5,5 @@ session_start();
 $con = mysqli_connect('localhost','root','','contactos');
 $sql = "SELECT * FROM contactos;";
 $answere = mysqli_query($con, $sql);
-echo json_encode(mysqli_fetch_all($answere);
+echo json_encode(mysqli_fetch_all($answere));
 ?>
