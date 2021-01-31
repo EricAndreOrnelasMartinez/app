2670e05856a1e6b55178ccc40eb298055ffec48b
eric <ericcot2@gmail.com>
2021-01-30 21:37:46 -0600

    add

--- Inicio/inicio.js
+++ Inicio/inicio.js
@@ -26,7 +26,7 @@ fetch('../PHP/contacts.php',{
 .then(res => res.json())
 .then(data => {
     let output = '';
-    console.log(data)
+    console.log(data);
     for(let i in data){
         output += `<div class="con">
         <p>Nombre: ${data[i].Nombre}</p><br>
